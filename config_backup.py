b2199c1ba3c74df301eb6ccbbd9fbca2487c4beb
Jon <jmstaley@gmail.com>
2011-03-23 00:50:05 +0000

    Save and load options to config file and implement simple backup

--- config_backup.py
+++ config_backup.py
@@ -5,10 +5,14 @@ class CbBackup(object):
         self.files = []
         self.builder = gtk.Builder()
         self.builder.add_from_file('gtk-ui.glade')
-        self.builder.connect_signals({'gtk_main_quit': gtk.main_quit})
+        self.builder.connect_signals({'gtk_main_quit': gtk.main_quit,
+                                      'on_backup_btn_clicked': self.backup_clicked})
 
         self.window = self.builder.get_object('main_window')
         self.window.show()
+
+    def backup_clicked(self, evt):
+        print "backup"
 
 if __name__ == '__main__':
     app = CbBackup()
