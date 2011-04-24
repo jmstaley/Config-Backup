f63284d04bfc407f1e5d73382614c5b04d13b97b
Jon Staley <jon@spandexbob.co.uk>
2011-04-24 18:54:44 +0100

    Add progressbar to app

--- config_backup.py
+++ config_backup.py
@@ -52,6 +52,9 @@ class Config(object):
             config.write(configfile)
 
 class Backup(object):
+    def __init__(self, copy_finished_callback):
+        self.copy_finished = copy_finished_callback
+
     def backup_files(self, files, target):
         if not os.path.exists(target):
             os.makedirs(target)
@@ -82,7 +85,8 @@ class Backup(object):
             if not path.endswith('/'):
                 path = '%s/' % path
             old_dir = '%s%s' % (path, d)
-            new_dir = '%s/%s' % (target,
+            new_dir = '%s/%s/%s' % (target,
+                    file_path.split('/')[-1],
                     old_dir.split(file_path)[-1])
             if not os.path.exists(new_dir):
                 os.mkdir(new_dir)
@@ -98,14 +102,14 @@ class Backup(object):
         dest = gio.File(path=target_path)
         src.copy_async(dest, self.copy_finished, flags=gio.FILE_COPY_OVERWRITE|gio.FILE_COPY_ALL_METADATA)
 
-    def copy_finished(self, source, result):
-        x = source.copy_finish(result)
 
 class BackupUI(object):
     def __init__(self):
         self.config = Config()
         self.user_home = os.getenv('HOME')
-        self.backup = Backup()
+        self.backup = Backup(self.copy_finished)
+        self.number_files = 0
+        self.files_done = 0
 
         self.builder = gtk.Builder()
         self.builder.add_from_file('gtk-ui.glade')
@@ -125,6 +129,9 @@ class BackupUI(object):
         self.target_entry.set_text(self.config.target)
 
         self.about = self.builder.get_object('aboutdialog')
+
+        self.progress_dialog = self.builder.get_object('progress_dialog')
+        self.pb = self.builder.get_object('progressbar')
 
         self.window = self.builder.get_object('main_window')
         self.window.show()
@@ -149,19 +156,53 @@ class BackupUI(object):
                 target=self.target_entry.get_text())
         gtk.main_quit()
 
-    def backup_clicked(self, evt):
+    def _get_files(self, files):
+        """ spider through the file options given and return a 
+            list of files to be copied """
         files_to_backup = []
+
+        for file_path in files:
+            if not file_path.startswith('/'):
+                file_path = '%s/%s' % (os.getenv('HOME'), file_path)
+
+            if os.path.isdir(file_path):
+                dirs_to_copy = []
+                files_to_copy = []
+                for path, dirs, files in os.walk(file_path):
+                    new_loc = path.split(file_path)[-1]
+                    for f in files:
+                        src = '%s/%s' % (path, f)
+                        files_to_backup.append(src)
+            else:
+                files_to_backup.append(file_path)
+        return files_to_backup 
+
+    def backup_clicked(self, evt):
+        file_options = []
 
         for check in self._get_active_checkboxes():
             file_path = '%s/%s' % (self.user_home, check.get_label())
-            files_to_backup.append(file_path)
+            file_options.append(file_path)
 
         for file_path in self.other_files.get_text().split(','):
             if file_path:
                 file_path = file_path.strip()
-                files_to_backup.append(file_path)
+                file_options.append(file_path)
 
-        self.backup.backup_files(files_to_backup, self.target_entry.get_text())
+        files_to_backup = self._get_files(file_options)
+        self.number_files = len(files_to_backup)
+
+        self.backup.backup_files(file_options, self.target_entry.get_text())
+        self.progress_dialog.show()
+
+    def copy_finished(self, source, result):
+        x = source.copy_finish(result)
+        self.pb.set_text('Finished %s' % source.get_basename())
+        self.files_done += 1
+        frac = float(float(self.files_done)/float(self.number_files))
+        self.pb.set_fraction(frac)
+        if frac == 1.0:
+            self.progress_dialog.destroy()
 
     def about_clicked(self, evt):
         self.about.connect('response', lambda d, r: d.destroy())
